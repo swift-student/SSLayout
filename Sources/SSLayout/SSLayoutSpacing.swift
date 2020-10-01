cf9cb69283323bd95bb43d106e3fc2d9cce1e302
Shawn Gee <shawngee@gmail.com>
2020-10-01 16:39:08 -0400

    make SSLayoutSpacing expressible by float and int literals

--- Sources/SSLayout/SSLayoutSpacing.swift
+++ Sources/SSLayout/SSLayoutSpacing.swift
@@ -17,4 +17,20 @@ public enum SSLayoutSpacing {
     case system(multiplier: CGFloat = 1, _ modifier: Modifier? = nil)
 }
 
+extension SSLayoutSpacing: ExpressibleByFloatLiteral {
+    public typealias FloatLiteralType = CGFloat.FloatLiteralType
+    
+    public init(floatLiteral value: Self.FloatLiteralType) {
+        self = .equalTo(CGFloat(floatLiteral: value))
+    }
+}
+
+extension SSLayoutSpacing: ExpressibleByIntegerLiteral {
+    public typealias IntegerLiteralType = CGFloat.IntegerLiteralType
+    
+    public init(integerLiteral value: CGFloat.IntegerLiteralType) {
+        self = .equalTo(CGFloat(integerLiteral: value))
+    }
+}
+
 #endif
